dadc386d9f680860da60d7589f3cd3df582d7ed9
Zachary Baum <zacharybaum42@gmail.com>
2016-09-30 13:23:49 -0400

    pass all tests for Brand

--- src/Brand.php
+++ src/Brand.php
@@ -31,6 +31,28 @@
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
+        function addStore($new_store)
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands_stores (brand_id, store_id) VALUES ({$this->getId()}, {$new_store->getId()});");
+        }
+
+        function getStores()
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT stores.* FROM brands
+                JOIN brands_stores ON (brands_stores.brand_id = brands.id)
+                JOIN stores ON (stores.id = brands_stores.store_id)
+                WHERE brands.id = {$this->getId()};");
+            $stores = array();
+            foreach ($returned_stores as $store)
+            {
+                $name = $store['name'];
+                $id = $store['id'];
+                $new_store = new store($name, $id);
+                array_push($stores, $new_store);
+            }
+            return $stores;
+        }
+
         static function getAll()
         {
             $got_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
@@ -48,6 +70,18 @@
         {
             $GLOBALS['DB']->exec("DELETE FROM brands;");
             $GLOBALS['DB']->exec("DELETE FROM brands_stores;");
+        }
+
+        static function find($search_id)
+        {
+            $found_brand = null;
+            $brands = Brand::getAll();
+            foreach ($brands as $brand) {
+                if ($brand->getId() == $search_id) {
+                    $found_brand = $brand;
+                }
+            }
+            return $found_brand;
         }
     }
  ?>
--- tests/brandTest.php
+++ tests/brandTest.php
@@ -68,5 +68,65 @@
             // Assert
             $this->assertEquals([], Brand::getAll());
         }
+
+        function test_find()
+        {
+            // Arrange
+            $name = "timberland";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $name2 = "nike";
+            $test_brand2 = new Brand($name2);
+            $test_brand2->save();
+
+            // Act
+            $result = Brand::find($test_brand2->getId());
+
+            // Assert
+            $this->assertEquals($test_brand2, $result);
+        }
+
+        function test_addStore()
+        {
+            // Arrange
+            $name = "timberland";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $sName = "DSW";
+            $test_store = new Store($sName);
+            $test_store->save();
+
+            // Act
+            $test_brand->addStore($test_store);
+
+            // Assert
+            $this->assertEquals([$test_store], $test_brand->getStores());
+        }
+
+        function test_getStores()
+        {
+            // Arrange
+            $name = "timberland";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $sName = "DSW";
+            $test_store = new Store($sName);
+            $test_store->save();
+            $test_brand->addStore($test_store);
+
+            $sName2 = "DSW";
+            $test_store2 = new Store($sName2);
+            $test_store2->save();
+            $test_brand->addStore($test_store2);
+
+            // Act
+            $result = $test_brand->getStores();
+
+            // Assert
+            $this->assertEquals([$test_store, $test_store2], $result);
+        }
     }
  ?>
